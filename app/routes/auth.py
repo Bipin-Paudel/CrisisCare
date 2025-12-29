ef1acd5e7fb386590576a956d09f241b6007f55f
SudeepSigdel <sudeepsigdel123@gmail.com>
2025-12-30 02:25:48 +0545

    fixing the login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, status, APIRouter
+from fastapi import HTTPException, status, APIRouter, Depends
 from sqlmodel import select
 from .. import models, schemas, utils, database
 from ..oauth2 import create_access_token
@@ -23,7 +23,7 @@ async def register_user(user: schemas.UserCreate, db: database.SessionLocal):
     return db_user
 
 @auth_router.post("/login", response_model=schemas.Token)
-def login_user(user: OAuth2PasswordRequestForm, db: database.SessionLocal):
+def login_user(db: database.SessionLocal, user: OAuth2PasswordRequestForm = Depends()):
     db_user = db.exec(select(models.User).where(models.User.email == user.username)).first()
     if not db_user or not utils.verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
