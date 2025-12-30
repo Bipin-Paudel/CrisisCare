a88beb78bb495192f7fbd361875fff54e37ba1a3
Bipin Paudel <paudelbipin19@gmail.com>
2025-12-30 23:23:03 +0545

    changes maded

--- crisisfront/src/Layout.jsx
+++ crisisfront/src/Layout.jsx
@@ -1,21 +1,16 @@
-import React from "react";
-import Navbar from "./Compontnts/Navbar/Nav";
-import Footer from "./Compontnts/Footer/Footer";
+import Navbar from "./components/Nav";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-slate-50">
-      
-      {/* Header */}
       <Navbar />
 
-      {/* Main Content */}
       <main className="flex-1">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
--- crisisfront/src/main.jsx
+++ crisisfront/src/main.jsx
@@ -1,56 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.Jsx'
-import Home from './Compontnts/Home/Home'
-import About from './Compontnts/AboutServices/About'
-import Login from './Compontnts/LoginRegister/Login'
-import Signup from './Compontnts/LoginRegister/Signup'
-import Contact from './Compontnts/Contact/Contact'
-import Gethelp from './Compontnts/Help/Gethelp'
-import Services from './Compontnts/AboutServices/Services'
-import Donate from './Compontnts/Donation/Donate'
-import Requestresource from './Compontnts/RequestResource/Requestresource'
-import PrivacyPolicy from './Compontnts/PrivacyPolicy/PrivacyPolicy.jsx'
-import TermsAndServices from './Compontnts/TermsService/TermsAndServices.jsx'
-import Donatedirectly from './Compontnts/Donation/Donatedirectly.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+import Layout from "./Layout.jsx";
 
+// Pages
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Services from "./components/Services.jsx";
 
-const router =createBrowserRouter(
- createRoutesFromElements(
-   <Route path='/' element={<Layout/>}>
-   <Route  path='' element={<Home/>} />
-   <Route  path='signup/' element={<Signup/>} />
-   <Route  path='about/' element={<About/>} />
-   <Route  path='login/' element={<Login/>} />
-   <Route  path='contact/' element={<Contact/>} />
-   <Route  path='gethelp/' element={<Gethelp/>} />
-   <Route  path='services/' element={<Services/>} />
-   <Route  path='donate/' element={<Donate/>} />
-   <Route path="/donate/:requestId" element={<Donatedirectly />} />
-   <Route  path='requestresource/' element={<Requestresource/>} />
-   <Route path="/privacy" element={<PrivacyPolicy/>} />
-   <Route path="/terms" element={<TermsAndServices/>} />
-   
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
+import Contact from "./components/Contact.jsx";
+import Gethelp from "./components/Gethelp.jsx";
 
-   </Route>
+import Donate from "./components/Donate.jsx";
+import Donatedirectly from "./components/Donatedirectly.jsx";
 
- )
+import Requestresource from "./components/Requestresource.jsx";
+import PrivacyPolicy from "./components/PrivacyPolicy.jsx";
+import TermsAndServices from "./components/TermsAndServices.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
 
-)
+      <Route path="signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
 
-createRoot(document.getElementById('root')).render(
+      <Route path="about" element={<About />} />
+      <Route path="services" element={<Services />} />
+
+      <Route path="contact" element={<Contact />} />
+      <Route path="gethelp" element={<Gethelp />} />
+
+      <Route path="donate" element={<Donate />} />
+      <Route path="donate/:requestId" element={<Donatedirectly />} />
+
+      <Route path="requestresource" element={<Requestresource />} />
+      <Route path="privacy" element={<PrivacyPolicy />} />
+      <Route path="terms" element={<TermsAndServices />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}>
-
-
-
-    </RouterProvider>
-    
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
